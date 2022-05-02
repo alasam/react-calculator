fd9a52dcf3ba32f66a9766ea6c49652267283543
Anthony <alasam@live.com>
2022-05-02 04:57:26 -0400

    Add buttons to calculator in app.js

--- src/App.js
+++ src/App.js
@@ -9,8 +9,35 @@ const App = () => {
     <Wrapper>
       <Screen value="0" />
       <ButtonBox>
-        {/* When Button clicked, console log test prints */}
-        <Button className="" value="0" onClick={() => {console.log("test")}} />
+        {/* first row of buttons */}
+        <Button className="" value="C" onClick={() => {console.log("CLEAR")}} />
+        <Button className="" value="+-" onClick={() => {console.log("+-")}} />
+        <Button className="" value="%" onClick={() => {console.log("%")}} />
+        <Button className="" value="/" onClick={() => {console.log("Divide")}} />
+
+        {/* second row of buttons */}
+        <Button className="" value="7" onClick={() => {console.log("7")}} />
+        <Button className="" value="8" onClick={() => {console.log("8")}} />
+        <Button className="" value="9" onClick={() => {console.log("9")}} />
+        <Button className="" value="X" onClick={() => {console.log("X")}} />
+
+        {/* third row of buttons */}
+        <Button className="" value="4" onClick={() => {console.log("4")}} />
+        <Button className="" value="5" onClick={() => {console.log("5")}} />
+        <Button className="" value="6" onClick={() => {console.log("6")}} />
+        <Button className="" value="-" onClick={() => {console.log("-")}} />
+
+        {/* forth row of buttons */}
+        <Button className="" value="1" onClick={() => {console.log("1")}} />
+        <Button className="" value="2" onClick={() => {console.log("2")}} />
+        <Button className="" value="3" onClick={() => {console.log("3")}} />
+        <Button className="" value="+" onClick={() => {console.log("+")}} />
+
+        {/* fifth row of buttons */}
+        <Button className="" value="0" onClick={() => {console.log("0")}} />
+        <Button className="" value="." onClick={() => {console.log(".")}} />
+        <Button className="equals" value="=" onClick={() => {console.log("=")}} />
+        
       </ButtonBox>
     </Wrapper>
   );
