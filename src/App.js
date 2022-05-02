c8145f63857570aed8d3df14b0a323427d9ee0fd
Anthony <alasam@live.com>
2022-05-02 04:25:24 -0400

    Add comments to index.js, added base code to App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import Wrapper from "./components/Wrapper";
+import ButtonBox from "./components/ButtonBox";
+import Button from "./components/Button";
+import Screen from "./components/Screen";
 
-function App() {
+// Base code, for now
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <Screen value="0" />
+      <ButtonBox>
+        {/* When Button clicked, console log test prints */}
+        <Button className="" value="0" conClick={() => {console.log("test")}} />
+      </ButtonBox>
+    </Wrapper>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
 
+// Base code, for now
 ReactDOM.render(
   <React.StrictMode>
     <App />
